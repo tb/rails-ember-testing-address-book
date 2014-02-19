86e586f74fad2d3849b58f378cae7672f2b8e4e2
Tomasz Bak <t.bak@selleo.com>
2014-02-19 09:45:22 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AddressBook::Application.config.secret_key_base = 'e89d81f52b53844c0d512008242ac2684d68d20aae4da5600338bd4a7950a2c03a74b83cc7a0b3c6316dbad0e04b62f3a6a02129c23328ca04a6ad76079351a5'
